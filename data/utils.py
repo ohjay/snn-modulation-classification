b641649b3d1062c1be44bd7ba47f4d1c81be72fc
Owen Jow <owenjow@berkeley.edu>
2020-04-25 09:45:19 -0700

    vectorize / don't use random binomial (slow)

--- data/utils.py
+++ data/utils.py
@@ -7,51 +7,27 @@ def to_one_hot(t, width):
     return t_onehot.scatter_(1, t.unsqueeze(-1), 1)
 
 
-def __gen_ST(N, T, rate, mode='regular'):
-    if mode == 'regular':
-        spikes = np.zeros([T, N])
-        spikes[::(1000//rate)] = 1
-        return spikes
-    elif mode == 'poisson':
-        spikes = np.ones([T, N])
-        spikes[np.random.binomial(
-            1, float(1000. - rate)/1000, size=(T, N)).astype('bool')] = 0
-        return spikes
-    else:
-        raise Exception('mode must be regular or Poisson')
-
-
-def spiketrains(N, T, rates, mode='poisson'):
-    if not hasattr(rates, '__iter__'):
-        return __gen_ST(N, T, rates, mode)
-    rates = np.array(rates)
-    M = rates.shape[0]
-    spikes = np.zeros([T, N])
-    for i in range(M):
-        if int(rates[i]) > 0:
-            spikes[:, i] = __gen_ST(1, T, int(rates[i]), mode=mode).flatten()
-    return spikes
-
-
 def image2spiketrain(x, y, input_shape, target_size, gain=50, min_duration=None, max_duration=500):
     if min_duration is None:
         min_duration = max_duration - 1
+
     batch_size = x.shape[0]
-    T = np.random.randint(min_duration, max_duration, batch_size)
     Nin = np.prod(input_shape)
-    allinputs = np.zeros([batch_size, max_duration, Nin])
-    for i in range(batch_size):
-        st = spiketrains(T=T[i], N=Nin, rates=gain *
-                         x[i].reshape(-1)).astype(np.float32)
-        allinputs[i] = np.pad(
-            st, ((0, max_duration - T[i]), (0, 0)), 'constant')
-    allinputs = np.transpose(allinputs, (1, 0, 2))
-    allinputs = allinputs.reshape(
-        allinputs.shape[0], allinputs.shape[1], *input_shape)
+    rates = gain * x.reshape(batch_size, -1)
+    p = (1000.0 - np.array(rates)) / 1000  # (batch_size, Nin)
+    T = np.random.randint(min_duration, max_duration, batch_size)
 
-    alltgt = np.zeros(
-        [max_duration, batch_size, target_size], dtype=np.float32)
+    # Generate spike trains
+    all_inputs = np.zeros((max_duration, batch_size, Nin))
     for i in range(batch_size):
-        alltgt[:, i, :] = y[i]
+        spikes = np.ones((T[i], Nin))
+        spikes[(np.random.uniform(size=(T[i], Nin)) < p[i]).astype('bool')] = 0
+        all_inputs[:T[i], i, :] = spikes
 
-    return allinputs, alltgt
+    # The shape of `all_inputs` is (max_duration, batch_size, c, h, w)
+    all_inputs = all_inputs.reshape(max_duration, batch_size, *input_shape)
+
+    # The shape of `all_target` is (max_duration, batch_size, target_size)
+    all_target = np.repeat(y[np.newaxis, :, :], max_duration, axis=0)
+
+    return all_inputs, all_target
