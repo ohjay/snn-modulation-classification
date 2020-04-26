2bd3279c9bd456c8e94f278b7dc15f4ee0b11e67
Owen Jow <owenjow@berkeley.edu>
2020-04-25 19:19:33 -0700

    balance data wrt SNR as well

--- data/load_radio_ml.py
+++ data/load_radio_ml.py
@@ -12,11 +12,13 @@ class RadioMLDataset(data.Dataset):
     Available here: https://www.deepsig.io/datasets.
     """
 
-    def __init__(self, data_dir, train, normalize=True, per_class_size=10000, val_step=500):
+    def __init__(self, data_dir, train,
+                 normalize=True, min_snr=6, per_h5_frac=0.5, train_frac=0.9):
+
         self.train = train
 
-        if not os.path.exists(os.path.join(data_dir, 'class23.hdf5')):
-            # Split huge HDF5 file into per-class HDF5 files
+        if not os.path.exists(os.path.join(data_dir, 'class23_snr30.hdf5')):
+            # Split huge HDF5 file into per-SNR, per-class HDF5 files
 
             data_path = os.path.join(data_dir, 'GOLD_XYZ_OSC.0001_1024.hdf5')
             full_h5f = h5py.File(data_path, 'r')
@@ -31,43 +33,71 @@ class RadioMLDataset(data.Dataset):
             # the first ~1/24th consists of examples of the first class,
             # the second ~1/24th consists of examples of the second class, and so on.
 
+            # There are 24 different modulation classes.
+            # For each class, there are 2555904 / 24 = 106496 examples.
+            # There are 26 different SNR levels: -20, -18, -16, ..., 28, 30.
+            # For each (class, SNR) pair, there are 106496 / 26 = 4096 examples.
+
             # Convert one-hot labels back to argmax
             Y = np.argmax(full_h5f['Y'], axis=1)
             for class_idx in range(24):
-                class_h5f_path = os.path.join(data_dir, 'class%d.hdf5' % class_idx)
-                class_h5f = h5py.File(class_h5f_path, 'w')
-                class_h5f.create_dataset('X', data=full_h5f['X'][Y == class_idx, :, :])
-                class_h5f.create_dataset('Y', data=Y[Y == class_idx])
-                class_h5f.close()
-                print('Wrote class %d data to `%s`.' % (class_idx, class_h5f_path))
+                class_X = full_h5f['X'][Y == class_idx, :, :]
+                class_Z = full_h5f['Z'][Y == class_idx, 0]
+                for snr in range(-26, 32, 2):
+                    class_snr_name = 'class%d_snr%d.hdf5' % (class_idx, snr)
+                    h5f_path = os.path.join(data_dir, class_snr_name)
+                    h5f = h5py.File(h5f_path, 'w')
+                    h5f.create_dataset('X', data=class_X[class_Z == snr, :, :])
+                    h5f.close()
+                    print('Wrote (SNR {z}, class {cl}) data to `{path}`.'.format(
+                        z=snr, cl=class_idx, path=h5f_path))
+                class_X = None
+                class_Z = None
             Y = None
             full_h5f.close()
 
-        self.X = []
-        self.Y = []
+        # Min/max values across entire dataset
+        # Want to use the same values for train/test normalization
+        X_minval = float('inf')
+        X_maxval = float('-inf')
+
+        # The data for each (class, SNR) pair
+        # will be truncated to the first PER_H5_SIZE examples
+        per_h5_size = int(per_h5_frac * 4096)
+        snr_count = (30 - min_snr) // 2 + 1
+        train_split_size = int(train_frac * per_h5_size)
+        if train:
+            split_size = train_split_size
+        else:
+            split_size = per_h5_size - train_split_size
+        total_size = 24 * snr_count * split_size
+
+        self.X = np.zeros((total_size, 1024, 2), dtype=np.float32)
+        self.Y = np.zeros(total_size, dtype=np.int64)
         for class_idx in range(24):
-            class_h5f_path = os.path.join(data_dir, 'class%d.hdf5' % class_idx)
-            class_h5f = h5py.File(class_h5f_path, 'r')
-            self.X.append(class_h5f['X'][:per_class_size])
-            self.Y.append(class_h5f['Y'][:per_class_size])
-            class_h5f.close()
-        self.X = np.concatenate(self.X, axis=0)
-        self.Y = np.concatenate(self.Y, axis=0)
+            for snr in range(min_snr, 32, 2):
+                class_snr_name = 'class%d_snr%d.hdf5' % (class_idx, snr)
+                h5f_path = os.path.join(data_dir, class_snr_name)
+                h5f = h5py.File(h5f_path, 'r')
+                X = h5f['X'][:per_h5_size]
+                X_minval = min(X_minval, X.min())
+                X_maxval = max(X_maxval, X.max())
+                if train:
+                    X_split = X[:train_split_size]
+                else:
+                    X_split = X[train_split_size:]
+                # Interleave
+                self.X[class_idx*snr_count::24*snr_count] = X_split
+                self.Y[class_idx*snr_count::24*snr_count] = class_idx
+                h5f.close()
+                X = None
+                X_split = None
 
         # Add fake height dim (TODO remove if switching to 1D convolutions)
         self.X = self.X.transpose(0, 2, 1)[:, :, np.newaxis, :]
 
         if normalize:
-            minval = self.X.min()
-            self.X = (self.X - minval) / (self.X.max() - minval)
-
-        # Assign every (val_step)th signal/label to the validation set
-        if train:
-            self.X = self.X[np.arange(len(self.X)) % val_step != 0]
-            self.Y = self.Y[np.arange(len(self.Y)) % val_step != 0]
-        else:
-            self.X = self.X[::val_step]
-            self.Y = self.Y[::val_step]
+            self.X = (self.X - X_minval) / (X_maxval - X_minval)
 
     def __len__(self):
         return len(self.X)
