1de127153fb24da012fcaea0753b8dea570b6f05
Simon Kaufmann <sikaufma@ucsd.edu>
2020-05-01 12:25:54 +0000

    Add -u to python

--- scripts/train_mnist.sh
+++ scripts/train_mnist.sh
@@ -7,9 +7,12 @@ batch_size=128
 batch_size_test=512
 n_test_samples=1024
 
-python train.py \
+echo "train mnist" >> /lif/log2
+
+# -u to immediately print to stdout (for file redirect to see output before script ends)
+python train.py -u \
     --data $data \
     --network_spec $network_spec \
     --batch_size $batch_size \
     --batch_size_test $batch_size_test \
-    --n_test_samples $n_test_samples
+    --n_test_samples $n_test_samples > /lif/log-train
